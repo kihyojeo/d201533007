48b235983a81fd29daafad9eb9531b4ce9de6139
Kim, Hyojeong <kihyojeo@gmail.com>
2022-10-04 02:38:23 +0900

    Main.java 수정 #1

--- src/d201533007/Main.java
+++ src/d201533007/Main.java
@@ -3,7 +3,8 @@ package d201533007;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String s = "hello world";
+        System.out.println(s);
     }
 }
 
